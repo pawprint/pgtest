86f7c59a50c5cf8c09191a5ecdf651cec8e83a2f
Scott Baker <scott@pawprint.net>
2013-11-18 09:20:04 -0800

    Change to Single Page Application

--- js/main.js
+++ js/main.js
@@ -13,6 +13,17 @@ var app = {
         });
     },
 
+    renderHomeView: function() {
+        var html =
+                "<div class='header'><h1>Test App</h1></div>" +
+                "<div class='search-view'>" +
+                "<input class='search-key'/>" +
+                "<ul class='employee-list'></ul>" +
+                "</div>"
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -24,7 +35,7 @@ var app = {
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
+            self.renderHomeView();
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
