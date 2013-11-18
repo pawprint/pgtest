55de4216629a4e8526d97ffb949119e6dea2de67
Scott Baker <scott@pawprint.net>
2013-11-18 09:31:35 -0800

    Implement homeview class

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
 var app = {
-
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-
-    renderHomeView: function() {
-      $('body').html(this.homeTpl());
-      $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
