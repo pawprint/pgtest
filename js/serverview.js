4e71d3395e33a4ca17bb8e9e3e59d4277007cf55
Scott Baker <scott@pawprint.net>
2013-11-18 16:04:34 -0800

    First Shot at barcodescanner

--- js/serverview.js
+++ js/serverview.js
@@ -1,6 +1,7 @@
 var ServerView = function() {
     this.initialize = function() {
       this.el = $('<div/>');
+      $('.scanBC',this.el).on('click', this.scanCode);
 
       $.ajax({
         url:'http://www.pawprint.ca/inpost.php',
@@ -15,6 +16,20 @@ var ServerView = function() {
       });
       return false;
     };
+
+    this.scanCode = function() {
+        cordova.plugins.barcodeScanner.scan(
+          function (result) {
+              alert("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);
+          },
+          function (error) {
+              alert("Scanning failed: " + error);
+          }
+       );
+    }
 
     this.render = function() {
         this.el.html(ServerView.template());
